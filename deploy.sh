a49e10efdbbc2c5535ffceb1d4af0136435acc78
Yue-plus <Yue_plus@foxmail.com>
2021-01-27 14:19:17 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,9 @@ cd build
 # 如果是发布到自定义域名
 echo 'note.yueplus.ink' > CNAME
 
+git config --global user.name "yue-plus"
+git config --global user.email Yue_plus@foxmail.com
+
 git init
 git add -A
 git commit -m 'deploy'
